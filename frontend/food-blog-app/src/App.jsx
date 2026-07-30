9dcbcb9c04e731c1804aecf3207daee0b7548908
Shakti-195 <thakurshaktisingh195@gmail.com>
2026-07-30 12:16:27 +0530

    Update frontend for production

--- frontend/food-blog-app/src/App.jsx
+++ frontend/food-blog-app/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css'
 import {createBrowserRouter,RouterProvider} from "react-router-dom"
-import Home from './pages/Home'
+import Home from './Pages/Home'
 import MainNavigation from './components/MainNavigation'
 import axios from 'axios'
 import  AddFoodRecipe  from './pages/AddFoodRecipe'
